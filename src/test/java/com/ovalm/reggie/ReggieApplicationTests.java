3db0fb5425fef784b1aff7ecf7b5f64ae718a167
oval_m <1332256234@qq.com>
2024-02-19 23:38:24 +0800

    使用shardingsphere实现主从复制失败

--- src/test/java/com/ovalm/reggie/ReggieApplicationTests.java
+++ src/test/java/com/ovalm/reggie/ReggieApplicationTests.java
@@ -1,11 +1,16 @@
 package com.ovalm.reggie;
 
+import com.ovalm.reggie.entity.User;
+import com.ovalm.reggie.service.UserService;
 import com.ovalm.reggie.utils.POP3Util;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class ReggieApplicationTests {
@@ -14,6 +19,9 @@ class ReggieApplicationTests {
 	private POP3Util pop3Util;
 	@Autowired
 	private RedisTemplate<Object, Object> redisTemplate;
+
+	@Autowired
+	private UserService userService;
 
 	@Test
 	void testEmail() {
@@ -32,6 +40,16 @@ class ReggieApplicationTests {
 		valueOperations.set("111", "222");
 		String res = (String) valueOperations.get("111");
 		System.out.println(res);
+	}
+	@Resource
+	private DataSource dataSource;
+	@Test
+	void TestShardingsphere() {
+		User user = new User();
+		user.setId(1111L);
+		user.setName("11111");
+		user.setEmail("192321321@qq.com");
+		userService.save(user);
 	}
 
 }
